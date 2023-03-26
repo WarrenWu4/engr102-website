b750fda2bbd972afcb42aae6c91ad9116505f378
WarrenWu4 <warrenweiwu04@gmail.com>
2023-03-25 21:27:06 -0500

    added videos in tas data and completed review block header and reviewblock array in review

--- src/components/ReviewBlock.jsx
+++ src/components/ReviewBlock.jsx
@@ -1,14 +1,17 @@
 import "../styles/reviewblock.css"
 
-export default function ReviewBlock() {
+export default function ReviewBlock(props) {
+
+    const profile_img = (props.pic === "") ? "src/assets/tao_card_imgs/default.jpg":"src/assets/tao_card_imgs/"+props.pic;
+
     return (
         <div className="reviewblock-container">
 
 
             <div className="reviewblock-header">
-                <div className="reviewblock-profile"></div>
+                <div className="reviewblock-profile" style={{backgroundImage:`url(${profile_img}`}}></div>
                 <div className="reviewblock-bar">
-                    <div className="reviewblock-name"></div>
+                    <div className="reviewblock-name">{props.name}</div>
                     <div className="reviewblock-divider"></div>
                 </div>
             </div>
--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,15 +1,22 @@
 import "../styles/review.css"
 import Nav from "../components/Nav"
 import ReviewBlock from "../components/ReviewBlock"
+import data from "../tas.json"
 
 export default function Review() {
 
+    let reviewBlocks = [];
+    data.tas.map((ta) => {
+        if(typeof(ta.videos)!="undefined") {
+            reviewBlocks.push(<ReviewBlock vid={ta.videos} pic={ta.profilePic} name={ta.name}/>);
+        }
+    })
 
     return (
         <div className="review-page">
             <Nav/>
             <div className="review-title">Review Sessions</div>
-            <ReviewBlock/>
+            {reviewBlocks}
         </div>
     )
 }
