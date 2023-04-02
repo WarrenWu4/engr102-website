ae6f4e54561b6b3680cc80d2262fe5cd017f4cd5
WarrenWu4 <warrenweiwu04@gmail.com>
2023-04-02 14:23:11 -0500

    fixed ta name, finished/adjusted reviewblock size, added unit block and video hover effect

--- src/components/ReviewBlock.jsx
+++ src/components/ReviewBlock.jsx
@@ -1,8 +1,36 @@
 import "../styles/reviewblock.css"
+import UnitBlock from "./UnitBlock";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa"
 
 export default function ReviewBlock(props) {
 
     const profile_img = (props.pic === "") ? "src/assets/tao_card_imgs/default.jpg":"src/assets/tao_card_imgs/"+props.pic;
+
+    // TODO: change color for some units b/c hard to see w/ white text
+    const unitNumToColor = {
+        1:"#6096B4",
+        2:"#93BFCF",
+        3:"#EAC7C7",
+        4:"#E5FDD1",
+        5:"#C9F4AA",
+        6:"#D9ACF5",
+        7:"#AAE3E2",
+        8:"#7286D3"
+    }
+
+    // TODO: Add gallery functions and make sure videos are added/clickable
+    const vidGallery = props.vid.map((v) =>
+        <div className="video">
+            <div className="video-title">{v.title}</div>
+            <div className="video-date">{v.date}</div>
+            <div className="video-units">Units Covered:</div>
+            <div className="video-unit-container">
+                {v.units.map((unit) => 
+                    <UnitBlock num={unit} color={unitNumToColor[unit]}/>
+                )}
+            </div>
+        </div>
+    )
 
     return (
         <div className="reviewblock-container">
@@ -18,11 +46,11 @@ export default function ReviewBlock(props) {
 
 
             <div className="reviewblock-gallery">
-                <div className="reviewblock-left-btn"></div>
+                <div className="reviewblock-left-btn"><FaAngleLeft/></div>
                 <div className="reviewblock-video-container">
-                    <div className="video"></div>
+                    {vidGallery}
                 </div>
-                <div className="reviewblock-right-btn"></div>
+                <div className="reviewblock-right-btn"><FaAngleRight/></div>
             </div>
 
 
