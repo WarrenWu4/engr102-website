056a2298278fdcd33e819b2f87938bb4cffb1ec7
WarrenWu4 <warrenweiwu04@gmail.com>
2023-04-07 11:28:50 -0500

    completed home and navbar responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,25 +10,39 @@ export default function Home() {
             <Nav/> 
 
             <div className='hero-container'>
+
+
                 <div className='hero-left'>
-                    <div className='hero-left-wrapper center'>
-                        <div className='hero-header center splash-font'>
+
+                    <div className='hero-left-container center' style={{flexDirection: 'column'}}>
+
+                        <div className='hero-header center home-font-header'>
                             <div className='hero-header-text'>Howdy Aggie Engineers!</div>
                         </div>
-                        <div className='hero-subheader subsplash-font'>
+
+
+                        <div className='hero-subheader home-font-subheader'>
                             Welcome to the 102 website, <br></br>
                             Your guide through the 102 course
                         </div>
-                        <div className='start-btn btn-font center'>
+
+
+                        <div className='start-btn home-font-btn'>
                             <NavLink className={'center'} to="/learn">
                                 Get Started
                             </NavLink>
                         </div>
+
                     </div>
+
                 </div>
+
+
                 <div className='hero-right'>
                     <div className='hero-img bg-default-style'></div>
                 </div>
+
+
             </div>
 
             {/* add mouse trail effect b/c too boring for big screens */}
